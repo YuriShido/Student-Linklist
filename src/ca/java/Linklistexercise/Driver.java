194d3302151595cb29bb3ed25e39ff2515f9ad2e
YuriShido <sydonce57@gmail.com>
2020-11-12 17:22:49 -0800

    add case

--- src/ca/java/Linklistexercise/Driver.java
+++ src/ca/java/Linklistexercise/Driver.java
@@ -6,10 +6,22 @@ import java.util.Scanner;
 
 
 
+
+
 public class Driver {
 	
+	public static void displayStudentList(LinkedList<Student> students) {
+		if(!students.isEmpty()) {
+			Iterator<Student> iterator = students.iterator();
+			System.out.println("===students lists===");
+			while(iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+		} else System.err.println("There are no students in this list!");
+	}
 	
-	public static void addLinkedList(LinkedList<Student> students) {
+	
+	public static void addStudentToList(LinkedList<Student> students) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter your fullname");
 		String fullName = input.nextLine();
@@ -19,6 +31,7 @@ public class Driver {
 		int id = input.nextInt();
 		Student s4 = new Student(id, fullName, email);
 		students.add(s4);
+		displayStudentList(students);
 	}
 	
 
@@ -31,7 +44,7 @@ public class Driver {
 		while (iterator.hasNext()) {
 			Student s = iterator.next();
 			if (s.getId() == answerId) {
-				System.out.println("Removed: " + s + "\n");
+				System.out.println("Removed " + s + "\n");
 				iterator.remove(); 
 			}			
 //		Student finds;
@@ -62,14 +75,39 @@ public class Driver {
 		students.add(s1);
 		students.add(s2);
 		students.add(s3);
-		addLinkedList(students);
 		
+		System.out.println("--------Menu to-----------\n"
+				+ "1- add student\n"
+				+ "2- display students\n"
+				+ "3- remove student\n"
+				+ "4- quit\n"
+				+ "--------------------------\n");
+		int choice = input.nextInt();
+		boolean control = false;
+		while(!control){
+			switch(choice) {
+				case 1: addStudentToList(students);
+//				control = true;
+					break;
+				case 2: displayStudentList(students);
+//				control = true;
+					break;
+				case 3: removeStudent(students);
+//				control = true;
+					break;
+				case 4: System.out.println("Thank you! Good bye!");
+//				control = true;
+					break;
+				default :System.err.println("Invalid input");
+			} 
+			control = true;
+		} 
+		
+		
+//		addStudentToList(students);
 		//display the students list using Iterator
-		Iterator<Student> iterator = students.iterator();
-		while(iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
-		removeStudent(students);
+//		displayStudentList(students);
+//		removeStudent(students);
 	
 	}
 	
